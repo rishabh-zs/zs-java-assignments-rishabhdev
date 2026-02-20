a30b95a0bec887ecf47688ba987d4f505a6ac9b1
rishabh-zs <rishabh.dev@zopsmart.com>
2026-02-20 16:23:24 +0530

    RD | Assignment-3 | added .idea file in .gitignore

--- src/main/java/com/zs/assignment2/services/MatrixService.java
+++ src/main/java/com/zs/assignment2/services/MatrixService.java
@@ -23,6 +23,11 @@ public class MatrixService {
     }
 
 
+    /**
+     * Overloaded multiply method to handle scalar multiplication.
+     * Changed from 'scalarMultiply' to just 'multiply' per review feedback.
+     */
+    / Method 1: Matrix * Matrix
     public Matrix multiply(Matrix a, Matrix b) {
         if (a.getCols() != b.getRows()) return null;
         Matrix res = new Matrix(a.getRows(), b.getCols());
@@ -38,12 +43,20 @@ public class MatrixService {
         return res;
     }
 
-    public Matrix scalarMultiply(double k, Matrix a) {
+    / Method 2: scalar * Matrix
+    public Matrix multiply(double k, Matrix a) {
         Matrix res = new Matrix(a.getRows(), a.getCols());
         for (int i = 0; i < a.getRows(); i++)
             for (int j = 0; j < a.getCols(); j++)
                 res.setElement(i, j, a.getElement(i, j) * k);
         return res;
+    }
+
+    /**
+     * Overloaded multiply method (swapped parameters) for flexibility: Matrix * scalar
+     */
+    public Matrix multiply(Matrix a, double k) {
+        return multiply(k, a); // Simply calls the version above
     }
 
 
