bcd38aadce6cf6311954d6dde11f5f20f143e986
rishabh-zs <rishabh.dev@zopsmart.com>
2026-03-05 17:33:10 +0530

    RD | assignment 4 | added git log file

--- src/main/java/com/zs/assignment5/Main.java
+++ src/main/java/com/zs/assignment5/Main.java
@@ -14,14 +14,14 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("-----STARTING GIT LOG ANALYSIS--------");
         System.out.println();
-        String filePath = (args.length > 0) ? args[0] : "src/packages/sample_git.log";
+        String filePath = (args.length > 0) ? args[0] : "src/main/resources/log.txt";
         String dateStr = (args.length > 1) ? args[1] : "2026-02-20";
-        String fromDateStr = (args.length > 2) ? args[2] : dateStr;
-        String toDateStr = (args.length > 3) ? args[3] : dateStr;
+        String startdate = (args.length > 2) ? args[2] : dateStr;
+        String enddate = (args.length > 3) ? args[3] : dateStr;
 
         GitLogController controller = new GitLogController();
         if (args.length > 2) {
-            controller.processGitStats(filePath, dateStr, fromDateStr, toDateStr);
+            controller.processGitStats(filePath, dateStr, startdate, enddate);
         } else {
             controller.processGitStats(filePath, dateStr);
         }
